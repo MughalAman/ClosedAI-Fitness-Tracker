8027436d8a80170820f9046e895b7a26c979c7b4
trzero0 <84072977+trzero0@users.noreply.github.com>
2023-12-10 14:30:08 +0200

    Update MainPageWorkoutList.jsx

--- src/components/MainPageWorkoutList.jsx
+++ src/components/MainPageWorkoutList.jsx
@@ -134,7 +134,7 @@ const Workout = (props) => {
         textAlign: "center",
         position: "relative",
         cursor: "pointer",
-        width: '478px',
+        width: '600px',
         height: '555px',
         borderRadius: '4px',
         fontSize: '30px',
