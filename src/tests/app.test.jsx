4dbaa7f244d11a104e36d387475395c054d41a40
trzero0 <84072977+trzero0@users.noreply.github.com>
2023-12-10 15:10:31 +0200

    Update app.test.jsx

--- src/tests/app.test.jsx
+++ src/tests/app.test.jsx
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react';
-import App from './app';
+import App from '../App.jsx';
 import { it } from 'vitest';
 
 it("renders without crashing", () => {
